f68606c2f92b431d7df51975aaec2daeea594d1f
YUVAJANANI M <yuvajananimuthukumar@gmail.com>
2024-09-09 11:29:10 +0530

    Time: 14 ms (5.84%), Space: 10.9 MB (98.05%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.c
+++ 0141-linked-list-cycle/0141-linked-list-cycle.c
@@ -1,25 +1,11 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     struct ListNode *next;
- * };
- */
-bool hasCycle(struct ListNode *head) {
-    if(head==NULL)
-    return false;
-
-  if(head-> next == NULL)
-        return false;
-   struct  ListNode* slow = head;
-   struct  ListNode* fast = head;
-     while(slow != NULL && fast != NULL){
-         fast = fast->next;
-         if(fast != NULL)
-             fast = fast->next;
-         slow = slow-> next;
-         if(slow == fast)
-             return true;
-     }
+bool hasCycle(struct ListNode *slow) {
+    if(!slow || !slow->next) return false;
+    struct ListNode *fast = slow;
+    while(fast && fast->next){
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow == fast)
+            return true;
+    }
     return false;
 }
